c7439ea587f8a9e9c448bdf72d579896b9b42cac
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-22 10:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	let obj = Math;
+    return Object.getOwnPropertyNames(obj).filter(function(property) {
+      return typeof obj[property] === 'function';
+    });
+    
+  }
+  
+  console.log(getAllMethods(Math));
 }
 
-alert(allMethods());
+// alert(allMethods());
